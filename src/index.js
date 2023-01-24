5829533a0016aa45fde1644e0d99629a68f22507
anastakozz <shvedagon@gmail.com>
2023-01-24 22:52:58 +0300

    feat: add reverse-int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+
+
 module.exports = function reverse (n) {
-  
+  let a = Math.abs(n).toString();
+  let i = a.length - 1;
+  let result = '';
+
+  while ( i >= 0) {
+    result = result + a[i];
+    i-- ;
+  }
+  return (result);
 }
+
+
+
